cd0dd88086d1e59f8b3e8c9b941ed304cfff0997
Igor NIR Chudov <nir@nir.org.ru>
2019-01-14 23:30:10 +0400

    Database writer implemented

--- Database.py
+++ Database.py
@@ -13,9 +13,20 @@ class db:
     __conn = ''
     __cursor = ''
 
-    __create_clients = 'CREATE TABLE {} ({})'
-    __create_products = 'CREATE TABLE {} ({})'
-    __create_sales = 'CREATE TABLE {} ({}) FOREIGN KEY clients.id, products.id'
+    __create_clients = '''
+    CREATE TABLE IF NOT EXISTS {} ({});
+    '''
+    __create_products = '''
+    CREATE TABLE IF NOT EXISTS {} ({});
+    '''
+    __create_sales = '''
+    CREATE TABLE IF NOT EXISTS {}
+    (
+        {},
+        FOREIGN KEY (client) REFERENCES clients(id),
+        FOREIGN KEY (product) REFERENCES products(id)
+    );
+    '''
 
     def __init__(self,
             path,
@@ -39,13 +50,19 @@ class db:
         self.__conn = sqlite3.connect(self.__path)
         self.__cursor = self.__conn.cursor()
 
-    def __attr_list(self, alist):
+    def __attr_list(self, alist, dots=False):
         '''
         Склеить лист колоночек для создания таблички.
         '''
         attrs = ''
-        for elem in alist:
-            attrs = attrs + ', ' + elem
+        if not dots:
+            for elem in alist:
+                attrs += elem + ' TEXT' + ', '
+        else:
+            for elem in alist:
+                attrs += ':' + elem + ', '
+        attrs = attrs[:-2]
+        return attrs
 
     def __create_table(self, tname, attrs, query):
         '''
@@ -55,6 +72,20 @@ class db:
         print('Creating table {}'.format(tname))
         print(query.format(tname, self.__attr_list(attrs)))
         self.__conn.execute(query.format(tname, self.__attr_list(attrs)))
+        self.__conn.commit()
+
+    def __dump_data(self, tname, attributes, objects):
+        query = 'INSERT INTO {} VALUES ({})'.format(tname, self.__attr_list(attributes, True))
+        print('Executing query: {}'.format(query))
+        for key, value in objects.items():
+            tmp_val = value.as_dict()
+            tmp_val['id'] = key
+            if 'product' in tmp_val:
+                tmp_val['product'] = list(self.__products.keys())[list(self.__products.values()).index(value.getProduct())]
+            if 'client' in tmp_val:
+                tmp_val['client'] = list(self.__clients.keys())[list(self.__clients.values()).index(value.getClient())]
+            self.__conn.execute(query, tmp_val)
+        self.__conn.commit()
 
     def save(self):
         '''
@@ -63,4 +94,8 @@ class db:
         self.__create_table('clients', self.__client_attributes, self.__create_clients)
         self.__create_table('products', self.__product_attributes, self.__create_products)
         self.__create_table('sales', self.__sale_attributes, self.__create_sales)
+
+        self.__dump_data('clients', self.__client_attributes, self.__clients)
+        self.__dump_data('products', self.__product_attributes, self.__products)
+        self.__dump_data('sales', self.__sale_attributes, self.__sales)
 
