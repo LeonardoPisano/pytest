b2c7446e219096b5e16f4a889501d5289b1a10f3
LeonardoPisano <43151886+LeonardoPisano@users.noreply.github.com>
2020-04-22 15:52:06 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3.6
+#! /usr/bin/env python3.6
 
-
-from functools import total_ordering
-import random
 from enum import IntEnum
+import random
+from functools import total_ordering
 
-class Deck(IntEnum):
+class Card(IntEnum):
     ONE = 1
     TWO = 2
     THREE = 3
@@ -27,70 +26,105 @@ class Suit(IntEnum):
     SPADES = 3
     CLUBS = 4
 
+@total_ordering
 class DeckCard:
-    def __init__(self, deck, suit):
-        self.deck = deck
+    def __init__(self, card, suit):
+        self.card = card
         self.suit = suit
 
     def __int__(self):
-        return self.deck
-        return self.suit
+        return self.card
 
-    def __str__(self):
-        return "Карта: ({}, {})".format(self.deck, self.suit)
+    def __lt__(self, other):
+        return self.card < other.card
 
-#Младшая карта
-@total_ordering
-def isMinor(_gt_):
-    def _gt_(self,other):
-        return self.int(card1.deck) > self.int(card2.deck)
-    return isMinor(_gt_)
+    def __eq__(self, other):
+        return self.card == other.card
 
-#Одна масть
-@total_ordering
-def sameSuit(_eq_):
-    def _eq_(self,other):
-        return self.card1.suit == self.card2.suit
-    return sameSuit(_eq_)
+    def __repr__(self):
+        suit = dict()
+        suit[1] = 'diamonds'
+        suit[2] = 'hearts'
+        suit[3] = 'spades'
+        suit[4] = 'clubs'
 
-#Первая карта бьёт вторую
-def beats(isMinor, sameSuit):
-    if (self.card1.suit == self.card2.suit) and (self.int(card1.deck) > self.int(card2.deck)):
-        return
-    else:
-        return
+        card = dict()
+        card[1] = 'one'
+        card[2] = 'two'
+        card[3] = 'three'
+        card[4] = 'four'
+        card[5] = 'five'
+        card[6] = 'six'
+        card[7] = 'seven'
+        card[8] = 'eight'
+        card[9] = 'nine'
+        card[10] = 'ten'
+        card[11] = 'jack'
+        card[12] = 'queen'
+        card[13] = 'king'
+        card[14] = 'ace'
 
-#Первая карта бьёт вторую с учетом козыря
-def beats2(beats, trump):
-    if (self.trump.suit == self.card1.suit) and (self.trump.suit != self.card2.suit):
-        return True
+        return '{} {}'.format(suit[int(self.suit)], card[int(self)])
 
-    elif (self.trump.suit == self.card2.suit) and (self.trump.suit != self.card1.suit):
+
+class Play:
+
+    def isMinor(self, card):
+        return int(card) <= 10
+
+    def sameSuit(self, card1, card2):
+        return card1.suit == card2.suit
+
+    def beats(self, card1, card2):
+        if (int(card1) > int(card2)) and (card1.suit == card2.suit):
+            return True
         return False
 
-    elif (self.trump.suit == self.card1.suit) and (self.trump.suit == self.card2.suit):
-        return self.int(card1.deck) > self.int(card2.deck)
+    def beats2(self, beats, card1, card2, trump):
+        if (card1.suit == trump) and (beats == True):
+            return True
+        elif (card1.suit == trump) and (card1.suit != card2.suit):
+            return True
+        elif (beats == True):
+            return True
+        return False
+        #if card1.suit == trump:
+            #return True
 
-    return True
+    def beatList(self, ca):
+        beatlist = list()
 
-def random_card_factory():
-    deck = random.choice(list(Deck))
-    suit = random.choice(list(Suit))
+        for ca in beatlist:
+            while ca < 10:
+                beatlist.append(ca)
+        return beatlist
 
-    return DeckCard(deck, suit)
+    def random_card_factory(self):
+        card = random.choice(list(Card))
+        suit = random.choice(list(Suit))
+
+        return DeckCard(card, suit)
+
+    def yes_no(self, result):
+        if result:
+            return "бьёт"
+        return "не бьёт"
+
 
 def main():
-    card1 = random_card_factory()
-    card2 = random_card_factory()
-    trump = random.choice(list(Suit))
-    print(trump)
+    a = Play()
+    ca = a.random_card_factory()
+    card1 = a.random_card_factory()
+    card2 = a.random_card_factory()
     print(card1)
     print(card2)
-    print('Первая карта младшая - {}'.format(int(card1.deck) < int(card2.deck)))
-    print('Карты одной масти - {}'.format(card1.suit == card2.suit))
-    print('Первая карта бьёт вторую - {}'.format((card1.suit == card2.suit) and (int(card1.deck) > int(card2.deck))))
-    print('Первая козырная карта бьёт вторую - {}'.format((card1.suit == card2.suit) and (int(card1.deck) < int(card2.deck))))
-    #print(beats2(beats, trump))
+
+    print('Младшая карта {}'.format(a.isMinor(card1)))
+    print('Карты одной масти {}'.format(a.sameSuit(card1, card2)))
+    print('Карта {} {} карту {}'.format(card1, a.yes_no(a.beats(card1, card2)), card2))
+    #print('Карта {} {} карт {}'.format(card1, yes_no(beats(card1, card2)), card2))
+    print('Карта {} {} карту {}'.format(card1, a.yes_no(a.beats2(a.beats(card1, card2), card1, card2, Suit.SPADES)), card2))
+    print(a.beatList(ca))
 
 if __name__ == '__main__':
     main()
